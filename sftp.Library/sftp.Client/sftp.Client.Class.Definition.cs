ef23bb673843bd09b013ecd69010134081714b4c
dgrads <51447297+dgrads@users.noreply.github.com>
2024-05-10 10:47:00 -0500

    Create sftp.Client.Class.Definition.cs

---
+++ sftp.Library/sftp.Client/sftp.Client.Class.Definition.cs
@@ -0,0 +1,11 @@
+namespace sftp.Client.Integration.Lib
+{
+    public class sftp.Client.Settings
+    {
+        public string RemoteHost { get; set; }
+        public string RemoteUser { get; set; }
+        public string PrivateKeyPath { get; set; }
+        public string LocalPath { get; set; }
+        public string RemotePath { get; set; }
+    }
+}
